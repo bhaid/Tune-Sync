ffd36c258f3aab724c5c63113f2de197b46a2015
Brendan <bhaid2006@icloud.com>
2023-06-21 12:04:13 -0400

    Initial Commit

---
+++ Syncify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Syncify
+//
+//  Created by Brendan Haidinger on 6/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
